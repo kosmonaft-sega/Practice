c20911cc6a7c6e3bd8ed6b0b515c28d89981fb61
kosmonaft-sega <alesharulitru13@gmail.com>
2026-09-23 21:46:59 +0500

    Add files via upload

--- Assets/rotationScript.cs
+++ Assets/rotationScript.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 public class rotationScript : MonoBehaviour
 {
     [SerializeField] private GameObject bladePrefab;
-    [SerializeField,Range(1,50)] int cubes = 8;
+    [SerializeField,Range(1,25)] int cubes = 8;
     [SerializeField,Range(0f,10f)] float radius = 5f;
     [SerializeField,Range(0f,500f)] float speed = 100f;
     [SerializeField] bool direction = true;
     [SerializeField,Range(0f,360f)] float angle = 360f;
-    int start_cubes;
+    int prev_cubes;
 
     void Awake()
     {
-        start_cubes = cubes;
+        prev_cubes = cubes;
         if(bladePrefab != null)
         {
-            for(float i = 0; i<angle; i+=angle/(float)start_cubes)
-            {
-                GameObject newobject = Instantiate(bladePrefab, transform.position, Quaternion.identity, transform);
-                newobject.transform.localEulerAngles = new Vector3(0f,i,0f);
-                newobject.transform.GetChild(0).transform.localPosition = new Vector3(radius,0f,0f);
-            }
+            summon_cube();
         }
         else
         {
@@ -32,6 +27,10 @@ public class rotationScript : MonoBehaviour
     {
         var a = direction ? 1f : -1f;
         transform.Rotate(new Vector3(0f,a*speed*Time.deltaTime,0f));
+        if (prev_cubes != cubes)
+        {
+            summon_cube();
+        }
     }
 
     void OnValidate()
@@ -43,8 +42,44 @@ public class rotationScript : MonoBehaviour
         int i = 0;
         foreach(Transform child in transform)
         {
-            child.localEulerAngles = new Vector3(0f,angle/(float)start_cubes*(float)i,0f);
+            child.localEulerAngles = new Vector3(0f,angle/(float)cubes*(float)i,0f);
             i++;
+        }
+    }
+
+    void summon_cube()
+    {
+        int children = transform.childCount;
+        GameObject newobject;
+        if(children<cubes)
+        {
+            for(int i=0; i<cubes; i++)
+            {
+                if(i<children)
+                {
+                    newobject = transform.GetChild(i).gameObject;
+                }
+                else
+                {
+                    newobject = Instantiate(bladePrefab, transform.position, Quaternion.identity, transform);
+                }
+                newobject.transform.localEulerAngles = new Vector3(0f,angle/(float)cubes*(float)i,0f);
+                newobject.transform.GetChild(0).transform.localPosition = new Vector3(radius,0f,0f);
+            }
+        }
+        else if(children>cubes)
+        {
+            int i = 0;
+            foreach(Transform child in transform)
+            {
+                child.transform.localEulerAngles = new Vector3(0f,angle/(float)cubes*(float)i,0f);
+                child.transform.GetChild(0).transform.localPosition = new Vector3(radius,0f,0f);
+                if(i>=cubes)
+                {
+                    Destroy(child.gameObject);
+                }
+                i++;
+            }
         }
     }
 }
